e6a7b9a05c74f28017544bcd9ec933da28712e1b
Mauricio Juárez <iconoclastsym@gmail.com>
2024-05-14 02:05:44 -0600

    including basic api code

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+from werkzeug.utils import secure_filename
+import numpy as np
+import os
+
+# Load your trained model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get data from Post request
+    data = request.get_json()
+    # Make sure the data is in a 2D array if it's a single sample
+    features = np.array([data['features']])
+    # Make prediction
+    prediction = model.predict(features)
+    # Return results
+    return jsonify({'prediction': prediction.tolist()})
+
+
+
+@app.route('/predict_csv', methods=['POST'])
+def predict_csv():
+    # Check if a file was posted
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join('C:\\Users\\icono\\AppData\\Local\\Temp', filename)  # change 'YourUsername' to your actual username
+        file.save(filepath)
+        # Load the CSV data
+        data = pd.read_csv(filepath)
+        # Make prediction
+        prediction = model.predict(data).tolist()
+        # Return results
+        return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
